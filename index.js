6b661a1b38d4bcd3e088e2248bb609518cca0f92
Isabelgarcia <isabelgmegino@gmail.com>
2020-12-02 20:14:21 +0100

    2part

--- index.js
+++ index.js
@@ -43,5 +43,41 @@ const getInfo = () => {
         })
 }
 
-console.log(getInfo())
+let urlCharacters = 'https://swapi.dev/api/people/'
+
+
+const getInfoCharacters = () => {
+    fetch(urlCharacters)
+        .then((res) => res.json())
+        .then((result) => {
+            //console.log(result.results)
+            let charactersArray = result.results.map((character) =>{
+              return character.name
+              })
+            let filmsArray = result.results.map((film) =>{
+              return film.films
+              })
+            let filmsArrayLength = filmsArray.map((characterInArray)=>{
+              return characterInArray.length
+            })
+            //console.log(filmsArrayLength,charactersArray)
+            let data = {
+                "series": [filmsArrayLength],
+                "labels": charactersArray
+              }
+            console.log(data)
+
+            new Chartist.Bar('#ct-chart-bar', data);
+
+        })
+        .catch((error) => {
+            console.log(error.message)
+        })
+}
+
+
+getInfo()
+
+getInfoCharacters()
+
 
